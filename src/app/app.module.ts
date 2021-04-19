76b03bacfa6f4ed6ce17795a7b340bd56849592c
connellrobert <robert.lel.connell@gmail.com>
2021-04-19 15:14:33 -0500

    adding components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './components/question/question.component';
+import { AnswerComponent } from './components/answer/answer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ParticipantLoginComponent } from './components/participant-login/participant-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    AnswerComponent,
+    HeaderComponent,
+    ParticipantLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/participant-login/participant-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participant-login',
+  templateUrl: './participant-login.component.html',
+  styleUrls: ['./participant-login.component.css']
+})
+export class ParticipantLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
